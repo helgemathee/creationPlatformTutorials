fa0cd6ef51b2fa3ae07aa6fe8c74532c235fdf39
Helge Mathee <helge.mathee@fabric-engine.com>
2012-08-21 17:21:01 +0100

    AssetViewer: Menu working + directory browser UI working

--- AssetViewer/AssetViewer.py
+++ AssetViewer/AssetViewer.py
@@ -1,13 +1,45 @@
+import os.path
 from FabricEngine.CreationPlatform.PySide.Widgets import *
 
 class AssetViewerApp(Application):
+  
+  __directory = None
 
   def __init__(self, **options):
     
     super(AssetViewerApp, self).__init__(**options)
     
+    # access mainwindow and menubar
+    mainWindow = self.getMainWindow()
+    menuBar = mainWindow.menuBar()
+    
+    # setup menu actions
+    fileMenu = menuBar.addMenu("File")
+    browseAction = fileMenu.addAction("Browse")
+    fileMenu.addSeparator()
+    quitAction = fileMenu.addAction("Quit")
+    
+    # connect menu action
+    browseAction.triggered.connect(self.browseDirectory)
+    quitAction.triggered.connect(self.close)
+    
+    self.__directory = os.path.abspath('.')
     
     self.constructionCompleted()
+    
+  def browseDirectory(self, directory = None):
+    
+    # if this is called from the menubar
+    if directory is None:
+      qtDir = QtGui.QFileDialog.getExistingDirectory(
+        self.getMainWindow(),
+        "Choose directory",
+        self.__directory)
+      if qtDir is None:
+        return
+      self.__directory = str(qtDir)
+    
+    print self.__directory
     
     
 app = AssetViewerApp()
