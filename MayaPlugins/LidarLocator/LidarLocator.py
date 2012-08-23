408a0b08732caff284517f2b819160963de95a30
Helge Mathee <helge.mathee@fabric-engine.com>
2012-08-23 12:08:07 +0100

    LidarLocator: Node skeleton

--- MayaPlugins/LidarLocator/LidarLocator.py
+++ MayaPlugins/LidarLocator/LidarLocator.py
@@ -9,6 +9,30 @@ import maya.cmds as mc
 # NOTE: USE THIS NODE ID!
 # node id = OpenMaya.MTypeId(0x0011AE60),
 
+class LidarLocator(OpenMayaMPx.MPxLocatorNode):
+  
+  __fileName = OpenMaya.MObject()
+  
+  def __init__(self):
+    super(LidarLocator, self).__init__()
+    
+  def __del__(self):
+    pass
+    
+  def compute(self, plug, datablock):
+    pass
+  
+  def draw(self, view, path, style, status):
+    pass
+  
+  @staticmethod
+  def creator():
+    return OpenMayaMPx.asMPxPtr( LidarLocator() )
+  
+  @staticmethod
+  def initializer():
+    return OpenMaya.MStatus.kSuccess
+
 # initialize the script plug-in
 def initializePlugin(mobject):
   """Loads the plugin"""
