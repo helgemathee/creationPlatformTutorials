fab1aafd30c6988bdac8897ba6176c74e0850b16
Helge Mathee <helge.mathee@fabric-engine.com>
2012-08-23 12:32:04 +0100

    LidarLocator: Register node + attribute init

--- MayaPlugins/LidarLocator/LidarLocator.py
+++ MayaPlugins/LidarLocator/LidarLocator.py
@@ -6,9 +6,6 @@ import maya.OpenMayaAnim as OpenMayaAnim
 import maya.OpenMayaRender as OpenMayaRender
 import maya.cmds as mc
 
-# NOTE: USE THIS NODE ID!
-# node id = OpenMaya.MTypeId(0x0011AE60),
-
 class LidarLocator(OpenMayaMPx.MPxLocatorNode):
   
   __fileName = OpenMaya.MObject()
@@ -31,14 +28,31 @@ class LidarLocator(OpenMayaMPx.MPxLocatorNode):
   
   @staticmethod
   def initializer():
+    tAttr = OpenMaya.MFnTypedAttribute()
+    
+    LidarLocator.__fileName = tAttr.create("fileName", "fn", OpenMaya.MFnData.kString)
+    tAttr.setStorable(1)
+    tAttr.setUsedAsFilename(1)
+    LidarLocator.addAttribute(LidarLocator.__fileName)
+    
     return OpenMaya.MStatus.kSuccess
 
 # initialize the script plug-in
 def initializePlugin(mobject):
   """Loads the plugin"""
   mplugin = OpenMayaMPx.MFnPlugin(mobject)
+  mplugin.registerNode(
+    "LidarLocator",
+    OpenMaya.MTypeId(0x0011AE60),
+    LidarLocator.creator,
+    LidarLocator.initializer,
+    OpenMayaMPx.MPxNode.kLocatorNode
+  )
   
 # uninitialize the script plug-in
 def uninitializePlugin(mobject):
   """Unloads the plugin"""
   mplugin = OpenMayaMPx.MFnPlugin(mobject)
+  mplugin.deregisterNode(
+    OpenMaya.MTypeId(0x0011AE60)
+  )
