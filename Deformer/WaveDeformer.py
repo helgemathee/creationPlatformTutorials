0db3efeabbc9e37afa2822851dfe6de6284d9fa5
Helge Mathee <helge.mathee@fabricengine.com>
2012-08-29 16:47:15 +0200

    wave deformer: basic component done

--- Deformer/WaveDeformer.py
+++ Deformer/WaveDeformer.py
@@ -4,6 +4,43 @@ from FabricEngine.CreationPlatform.Nodes.Kinematics import *
 from FabricEngine.CreationPlatform.Nodes.Lights import *
 from FabricEngine.CreationPlatform.Nodes.Rendering import *
 from FabricEngine.CreationPlatform.Nodes.Manipulation import *
+from FabricEngine.CreationPlatform.Nodes import Component
+from FabricEngine.CreationPlatform.RT.Math import *
+
+class WaveDeformComponent(Component):
+  
+  def _setDefaultOptions(self, options):
+    super(WaveDeformComponent, self)._setDefaultOptions(options)
+    options.setdefault('name', "wave")
+    options.setdefault('axis', Vec3(1.0, 0.0, 0.0))
+    options.setdefault('center', Vec3(0.0, 0.0, 0.0))
+    options.setdefault('amplitude', 1.0)
+    options.setdefault('frequency', 1.0)
+    
+  def apply(self, node):
+    super(WaveDeformComponent, self).apply(node)
+    
+    if not node.isTypeOf('PolygonMesh'):
+      raise Exception("Node not of type PolygonMesh!")
+    
+    # construct a unique name
+    name = self._getOption('name') + str(node.getNumComponents()) + '_'
+    
+    # access the core node
+    dgNode = node.getGeometryDGNode()
+    
+    # add all of the members
+    dgNode.addMember(name + 'axis', 'Vec3', self._getOption('axis'))
+    dgNode.addMember(name + 'center', 'Vec3', self._getOption('center'))
+    dgNode.addMember(name + 'amplitude', 'Scalar', self._getOption('amplitude'))
+    dgNode.addMember(name + 'frequency', 'Scalar', self._getOption('frequency'))
+    
+    # add interfaces
+    self._addMemberInterface(dgNode, name+'axis', True)
+    self._addMemberInterface(dgNode, name+'center', True)
+    self._addMemberInterface(dgNode, name+'amplitude', True)
+    self._addMemberInterface(dgNode, name+'frequency', True)
+    
 
 class MyApp(Basic3DDemoApplication):
   
@@ -35,10 +72,14 @@ class MyApp(Basic3DDemoApplication):
     
     # construct an instance
     instance = Instance(scene,
-      geometry = torus,
+      geometry = torus,   
       transform = transform,
       material = material
     )
+    
+    # apply wave deforms
+    deformer = WaveDeformComponent()
+    torus.addComponent(deformer)
     
     self.constructionCompleted()
 
